2c122634784e4e0df7046f76a612e67bafa65471
mykks32 <srikri5462ydv@gmail.com>
2025-04-03 15:56:42 +0545

    feat: final commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
-import Image from "next/image";
+import Header from "@/components/layouts/header";
+import HeroSection from "@/components/landing/hero-section";
+import FaqsSection from "@/components/landing/faqs-section";
+import FooterSection from "@/components/landing/footer-section";
 
 export default function Home() {
   return (
-    <div>
-      Hello World
+    <div className="flex flex-col">
+      <Header />
+      <HeroSection />
+      <FaqsSection />
+      <FooterSection />
     </div>
   );
 }
